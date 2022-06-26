b7af48a98d017f751049a9022e0d5abc4b58a33e
k0t9i <k0t9i@yandex.ru>
2022-06-26 23:21:07 +0400

    test: add test for membership and relationship application layer

---
+++ tests/unit/ProjectMemberships/Application/Handler/LeaveProjectCommandHandlerTest.php
@@ -0,0 +1,110 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\unit\ProjectMemberships\Application\Handler;
+
+use App\ProjectMemberships\Application\CQ\LeaveProjectCommand;
+use App\ProjectMemberships\Application\Handler\LeaveProjectCommandHandler;
+use App\ProjectMemberships\Domain\Entity\Membership;
+use App\ProjectMemberships\Domain\Exception\ProjectMembershipNotExistException;
+use App\ProjectMemberships\Domain\Repository\MembershipRepositoryInterface;
+use App\ProjectMemberships\Domain\ValueObject\MembershipId;
+use App\Shared\Domain\Bus\Event\DomainEvent;
+use App\Shared\Domain\Bus\Event\EventBusInterface;
+use App\Shared\Domain\ValueObject\UserId;
+use DG\BypassFinals;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+class LeaveProjectCommandHandlerTest extends TestCase
+{
+    private Generator $faker;
+
+    protected function setUp(): void
+    {
+        BypassFinals::setWhitelist(['*/src/ProjectMemberships/*']);
+        BypassFinals::enable();
+        $this->faker = Factory::create();
+    }
+
+    public function testPositive()
+    {
+        $membershipId = $this->faker->uuid();
+        $userId = $this->faker->uuid();
+
+        $membership = $this->getMockBuilder(Membership::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['removeParticipant', 'releaseEvents'])
+            ->getMock();
+        $membership->expects(self::once())
+            ->method('removeParticipant')
+            ->with(
+                self::equalTo(new UserId($userId)),
+                self::equalTo(new UserId($userId))
+            );
+        $releaseEventsResult = [
+            $this->getMockForAbstractClass(DomainEvent::class, callOriginalConstructor: false)
+        ];
+        $membership->expects(self::once())
+            ->method('releaseEvents')
+            ->willReturn($releaseEventsResult);
+
+        $membershipRepository = $this->getMockForAbstractClass(
+            MembershipRepositoryInterface::class,
+            mockedMethods: ['findById', 'update']
+        );
+        $membershipRepository->expects(self::once())
+            ->method('findById')
+            ->with(new MembershipId($membershipId))
+            ->willReturn($membership);
+        $membershipRepository->expects(self::once())
+            ->method('update')
+            ->with($membership);
+
+        $eventBus = $this->getMockForAbstractClass(
+            EventBusInterface::class,
+            mockedMethods: ['dispatch']
+        );
+        $eventBus->expects(self::once())
+            ->method('dispatch')
+            ->with(...$releaseEventsResult);
+
+        $handler = new LeaveProjectCommandHandler(
+            $membershipRepository,
+            $eventBus
+        );
+        $command = new LeaveProjectCommand($membershipId, $userId);
+
+        $handler->__invoke($command);
+    }
+
+    public function testNonExistingMembership(): void
+    {
+        $membershipId = $this->faker->uuid();
+        $userId = $this->faker->uuid();
+
+        $membershipRepository = $this->getMockForAbstractClass(
+            MembershipRepositoryInterface::class,
+            mockedMethods: ['findById']
+        );
+        $membershipRepository->expects(self::once())
+            ->method('findById')
+            ->with(new MembershipId($membershipId))
+            ->willReturn(null);
+
+        $eventBus = $this->getMockForAbstractClass(
+            EventBusInterface::class,
+        );
+
+        $handler = new LeaveProjectCommandHandler(
+            $membershipRepository,
+            $eventBus
+        );
+        $command = new LeaveProjectCommand($membershipId, $userId);
+
+        self::expectException(ProjectMembershipNotExistException::class);
+        $handler->__invoke($command);
+    }
+}
+
---
+++ tests/unit/ProjectRelationships/Application/Handler/DeleteLinkCommandHandlerTest.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\unit\ProjectRelationships\Application\Handler;
+
+use App\ProjectRelationships\Application\CQ\DeleteLinkCommand;
+use App\ProjectRelationships\Application\Handler\DeleteLinkCommandHandler;
+use App\ProjectRelationships\Domain\Entity\Relationship;
+use App\ProjectRelationships\Domain\Exception\ProjectRelationshipNotExistException;
+use App\ProjectRelationships\Domain\Repository\RelationshipRepositoryInterface;
+use App\ProjectRelationships\Domain\ValueObject\RelationshipTaskId;
+use App\Shared\Domain\Bus\Event\DomainEvent;
+use App\Shared\Domain\Bus\Event\EventBusInterface;
+use App\Shared\Domain\ValueObject\UserId;
+use DG\BypassFinals;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+class DeleteLinkCommandHandlerTest extends TestCase
+{
+    private Generator $faker;
+
+    protected function setUp(): void
+    {
+        BypassFinals::setWhitelist(['*/src/ProjectRelationships/*']);
+        BypassFinals::enable();
+        $this->faker = Factory::create();
+    }
+
+    public function testPositive()
+    {
+        $fromTaskId = $this->faker->uuid();
+        $toTaskId = $this->faker->uuid();
+        $userId = $this->faker->uuid();
+
+        $relationship = $this->getMockBuilder(Relationship::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['deleteLink', 'releaseEvents'])
+            ->getMock();
+        $relationship->expects(self::once())
+            ->method('deleteLink')
+            ->with(
+                self::equalTo(new RelationshipTaskId($fromTaskId)),
+                self::equalTo(new RelationshipTaskId($toTaskId)),
+                self::equalTo(new UserId($userId))
+            );
+        $releaseEventsResult = [
+            $this->getMockForAbstractClass(DomainEvent::class, callOriginalConstructor: false)
+        ];
+        $relationship->expects(self::once())
+            ->method('releaseEvents')
+            ->willReturn($releaseEventsResult);
+
+        $relationshipRepository = $this->getMockForAbstractClass(
+            RelationshipRepositoryInterface::class,
+            mockedMethods: ['findByTaskId', 'update']
+        );
+        $relationshipRepository->expects(self::once())
+            ->method('findByTaskId')
+            ->with(new RelationshipTaskId($fromTaskId))
+            ->willReturn($relationship);
+        $relationshipRepository->expects(self::once())
+            ->method('update')
+            ->with($relationship);
+
+        $eventBus = $this->getMockForAbstractClass(
+            EventBusInterface::class,
+            mockedMethods: ['dispatch']
+        );
+        $eventBus->expects(self::once())
+            ->method('dispatch')
+            ->with(...$releaseEventsResult);
+
+        $handler = new DeleteLinkCommandHandler(
+            $relationshipRepository,
+            $eventBus
+        );
+        $command = new DeleteLinkCommand($fromTaskId, $toTaskId, $userId);
+
+        $handler->__invoke($command);
+    }
+
+    public function testNonExistingRelationship(): void
+    {
+        $fromTaskId = $this->faker->uuid();
+        $toTaskId = $this->faker->uuid();
+        $userId = $this->faker->uuid();
+
+        $relationshipRepository = $this->getMockForAbstractClass(
+            RelationshipRepositoryInterface::class,
+            mockedMethods: ['findByTaskId']
+        );
+        $relationshipRepository->expects(self::once())
+            ->method('findByTaskId')
+            ->with(new RelationshipTaskId($fromTaskId))
+            ->willReturn(null);
+
+        $eventBus = $this->getMockForAbstractClass(
+            EventBusInterface::class,
+        );
+
+        $handler = new DeleteLinkCommandHandler(
+            $relationshipRepository,
+            $eventBus
+        );
+        $command = new DeleteLinkCommand($fromTaskId, $toTaskId, $userId);
+
+        self::expectException(ProjectRelationshipNotExistException::class);
+        $handler->__invoke($command);
+    }
+}
