67c5300a8534fd75d2a28d8cbaa5241768e49530
k0t9i <k0t9i@yandex.ru>
2022-07-25 18:05:31 +0400

    refactor: remove hydrator and storage classes

--- app/src/Shared/Application/Hydrator/Accessor/ValueAccessorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace App\Shared\Application\Hydrator\Accessor;
-
-interface ValueAccessorInterface
-{
-    public function getValue(?object $object = null): mixed;
-}
