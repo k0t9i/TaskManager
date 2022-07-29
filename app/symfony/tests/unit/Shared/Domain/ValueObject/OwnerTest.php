d35bb9cff4ce984c25325d8a6ec8267b19c41227
k0t9i <k0t9i@yandex.ru>
2022-07-29 23:46:02 +0400

    test: add tests for Owner

---
+++ app/symfony/tests/unit/Shared/Domain/ValueObject/OwnerTest.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\unit\Shared\Domain\ValueObject;
+
+use App\Shared\Domain\Exception\UserIsAlreadyOwnerException;
+use App\Shared\Domain\Exception\UserIsNotOwnerException;
+use App\Shared\Domain\ValueObject\Owner;
+use App\Shared\Domain\ValueObject\Users\UserId;
+use Faker\Factory;
+use Faker\Generator;
+use PHPUnit\Framework\TestCase;
+
+class OwnerTest extends TestCase
+{
+    private Generator $faker;
+    private string $ownerId;
+    private Owner $owner;
+
+    protected function setUp(): void
+    {
+        $this->faker = Factory::create();
+        $this->ownerId = $this->faker->uuid();
+        $this->owner = new Owner(new UserId($this->ownerId));
+    }
+
+    public function testEnsureIsNotOwner(): void
+    {
+        self::expectException(UserIsAlreadyOwnerException::class);
+        $this->owner->ensureIsNotOwner(new UserId($this->ownerId));
+    }
+
+    public function testIsOwner(): void
+    {
+        self::assertTrue($this->owner->isOwner(new UserId($this->ownerId)));
+        self::assertFalse($this->owner->isOwner(new UserId($this->faker->uuid())));
+    }
+
+    public function testEnsureIsOwner(): void
+    {
+        self::expectException(UserIsNotOwnerException::class);
+        $this->owner->ensureIsOwner(new UserId($this->faker->uuid()));
+    }
+}
+
