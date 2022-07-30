b631a6d385e9a73b06120865ee3c1bd2bc78e9b7
k0t9i <k0t9i@yandex.ru>
2022-07-30 15:50:24 +0400

    ci: add php cs fixer

---
+++ app/symfony/.php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        dirname(__DIR__).'/src/',
+        __DIR__.'/tests',
+    ]);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
